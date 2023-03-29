cdd97daf013879a24e8ae7b683986453668fafbb
Siddharth Kandpal <123426199+siddharthkandpal@users.noreply.github.com>
2023-03-29 03:56:43 -0400

    Update exercise2.cpp

--- dv/exercise2.cpp
+++ dv/exercise2.cpp
@@ -1,6 +1,69 @@
+#include <cstdint>
 #include <catch2/catch_test_macros.hpp>
 #include <VExercise2.h>
+#include <bit>
 
-TEST_CASE("Exercise 2") {
+
+//defining the linear fabionacci shift register as a struct and calling values
+
+struct LFSR {
+  uint16_t value;
+  static constexpr uint16_t mask {0xB400};
+
+  
+// iterating through the shift register 
+  
+  void step() {
+    uint16_t bits = value & mask;
+    value = (value << 1) | (std::popcount(bits) & 1);
+  }
+};
+
+
+
+void step(VExercise2& model) {
+  model.clk = 0;
+  model.eval();
+  model.clk = 1;
+  model.eval();
+}
+
+
+
+// reset = high, testing the value initially 
+
+void test_initial_value(uint16_t init) {
   VExercise2 model;
+  model.init = init;
+  model.nReset = 0;
+  step(model);
+  model.nReset = 1;
+
+  LFSR lfsr {init};
+
+  for(uint8_t cycles {0}; cycles < 100; ++cycles) {
+    REQUIRE(model.out == lfsr.value);
+    step(model);
+    lfsr.step();
+  }
+
+  model.nReset = 0;
+  step(model);
+  REQUIRE(model.out == init);
+}
+
+TEST_CASE("Initial value 0x00") {
+  test_initial_value(0x00);
+}
+
+TEST_CASE("Initial value 0xFF") {
+  test_initial_value(0xFF);
+}
+
+TEST_CASE("Initial value 0x55") {
+  test_initial_value(0x55);
+}
+
+TEST_CASE("Initial value 0xAA") {
+  test_initial_value(0xAA);
 }
